a4a908509e08e8e5f3ef951dc37b43d371cde05d
Rafael Domingos <rafael.domingos@beedoo.com.br>
2024-06-22 22:17:00 -0300

    Comenta 2 testes

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -20,7 +20,7 @@ describe('Register a User', () => {
       cy.title().should('be.equal', 'My Account – Automation Practice Site')
       cy.section('fim das pré-condições')
     })
-    it('registra com o campo "Email-id" vazio e senha válida', () => {
+    /*it('registra com o campo "Email-id" vazio e senha válida', () => {
       cy.step('preenche com o campo "Email address" vazio e o campo "Password" com senha válida')
       cy.get(reg_password).type(password)
       //cy.step('aguarda por 5 segundos')
@@ -31,8 +31,7 @@ describe('Register a User', () => {
       cy.get('div.woocommerce ul.woocommerce-error li')
         .should('be.visible')
         .and('contain', 'Please provide a valid email address')
-
-    })
+    })*/
     it('registra com o campo "Password" vazio', () => {
       cy.step('preenche o campo "Email address" com e-mail válido e deixa o campo "Password" vazio')
       cy.get(reg_email).type(user, { log: false } )
@@ -51,5 +50,19 @@ describe('Register a User', () => {
       cy.contains('Error: Please provide a valid email address.')
         .should('be.visible')
     })
+    /*it('registra com um email já existente', () => {
+      cy.step('preenche o campo "Email address" com e-mail já registrado')
+      cy.get(reg_email).type(existing_email)
+      cy.step('preenche o campo "Password" com senha válida')
+      cy.get(reg_password).type(password)
+      //cy.step('aguarda por 5 segundos')
+      //cy.wait(1000)
+      cy.step('clica no botão "Register"')
+      cy.get(register_button).click( { force: true } )
+      cy.step('verifica que a mensagem de erro esta correta e visível')
+      cy.get('div.woocommerce ul.woocommerce-error li')
+        .should('be.visible')
+        .and('contain', ' An account is already registered with your email address. Please login.')
+    })*/
   })
 })
